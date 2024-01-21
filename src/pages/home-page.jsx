c07b44291e99bf28633675d748d0a13cbe2f3c01
werdien jihed <informatique.jihed@gmail.com>
2024-01-21 15:55:31 +0100

    update home page content

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -4,24 +4,7 @@ export default () => {
   return (
     <div>
       <h2>Welcome</h2>
-      <p>
-        This is a very simple home page. Explore this app by visiting those
-        links.
-      </p>
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/items">Items</Link>
-        </li>
-      </ul>
+      <p>This is a very simple home page.</p>
     </div>
   );
 };
